d5f4dba857f66f162386db6c2dfb1e317daaf176
divviprameela-ui <divviprameela@gmail.com>
2026-06-01 16:54:26 +0530

    Add files via upload

---
+++ indexing,slicing.py
@@ -0,0 +1,42 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+... Enter "help" below or click "Help" above for more information.
+... #indexing
+... a="vijayawada"
+... a[1]
+... 'i'
+... a[0]
+... 'v'
+... a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+... 'vijaya'
+... #slicing
+... a="vijayawada is a place of devotion"
+... a[0:10]
+... 'vijayawada'
+... a[15:20]
+... ' plac'
+... a[15:21]
+... ' place'
+... #negative indexing
+... a="sri vidya"
+... a[-1]
+... 'a'
+... a[-2]
+... 'y'
+... a[-4]
+... 'i'
+... #negative slicing
+... a="pooja madam'
+... SyntaxError: unterminated string literal (detected at line 1)
+... a="pooja madam"
+... a[-1:-6]
+... ''
+... a[-6:]
+... ' madam'
+... a[11:]
+... ''
+... a[10:]
+... 'm'
+... a[-10:]
+... 'ooja madam'
