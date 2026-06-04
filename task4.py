0ab81780a76ffa110f5b4ec8fbd65dc72e8f7a70
divviprameela-ui <divviprameela@gmail.com>
2026-06-04 15:44:13 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,16 @@
+#name
+#id no
+#college
+#email id
+#mobile.no
+#percentage
+name=input("enter the name:")
+id_no=str(input("enter the id_no:"))
+email_id=input("enter email_id:")
+mobile_no=int(input("entr mobile_no:"))
+percentage=float(input("enter percentage:"))
+print("student name:",name)
+print("id_no",id_no)
+print("email_id:",email_id)
+print("mobile number:",mobile_no)
+print("CGPA:",percentage)
