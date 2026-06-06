0f6b261e1ff9980e1fbada4e362466e507d5ea33
divviprameela-ui <divviprameela@gmail.com>
2026-06-06 15:16:09 +0530

    Add files via upload

---
+++ coditions3.py
@@ -0,0 +1,304 @@
+#if-eslif
+#comparision
+'''a=10
+b=20
+if a<b:
+    print("true")
+elif b>a:
+    print("yes")
+else:
+    print("false")'''
+
+'''a=int(input("data:"))
+b=int(input("data:"))
+if a>b:
+    print("true")
+elif b>a:
+    print("yes")
+else:
+    print("false")'''
+
+
+
+'''a=int(input("what is data"))
+b=int(input("what is data"))
+if a>b:
+    print("true")
+elif b==a:
+    print("yes")
+else:
+    print("false")'''
+
+#logical
+
+
+'''a=10
+b=20
+if a>b and b>a:
+    print("true")
+elif b>a or a==b:
+    print("yes")
+else:
+    print("false")'''
+
+#identify
+
+'''a=10
+b=20
+if type(b) is int:
+    print("true")
+elif type(a) is not str:
+    print("yes")
+else:
+    print("false")'''
+
+'''a=int(input("value:"))
+b=int(input("value:"))
+if type(b) is str:
+    print("true")
+elif type(a) is not str:
+    print("yes")
+else:
+    print("false")'''
+
+#membership
+
+'''a=[10,20,30,40]
+b=int(input("enter the value"))
+if b in a:
+    print("is there")
+elif b not in a:
+    print("is not there")
+else:
+    print("the value is missing")'''
+'''a=10
+b=20
+if type(b) is str:
+    print("true")
+elif type(a) is not int:
+    print("yes")
+elif type(a) is int:
+    print('yah it is true')
+else:
+    print("false")'''
+
+#multiple if conditions
+
+'''a=10
+b=80
+if a<b:
+    print("greater")
+if b>a:
+    print("true")
+
+
+a=int(input("the value"))
+b=int(input("the value"))
+if a<b:
+    print("greater")
+if b>a:
+    print("true")
+
+a=10
+b=80
+if a==b:
+    print("greater")
+if b!=a:
+    print("true")
+if b>a:
+    print("it is correct")'''
+
+#logical
+
+'''a=10
+b=80
+if a<b and b>a:
+    print("greater")
+if b>a or a>b:
+    print("true")
+
+a=int(input("the value"))
+b=int(input("the value"))
+if a<b and b>a:
+    print("greater")
+if b>a or a>b:
+    print("true")'''
+#identify
+
+'''a=10
+b=80
+if type(a) is int:
+    print("true")
+if type(b) is not str:
+    print("true")
+
+
+a=int(input("data:"))
+b=int(input("data:"))
+if type(a) is int:
+    print("true")
+if type(b) is not str:
+    print("true")'''
+
+#membership
+
+'''a=[10,20,30,40,50]
+b=[20,90]
+if b in a:
+    print("is there ")
+if b not in a:
+    print("true")
+
+a=[10,20,30,40,50]
+b=int(input('values'))
+if b in a:
+    print("is there ")
+if b not in a:
+    print("true")'''
+
+#nested if
+'''a=6
+b=8
+if a==b:
+    print("true")
+    if a<b:
+        print("false")'''
+
+'''a=6
+b=8
+if a==b:
+    print("true")
+    if a<b:
+        print("false")
+    else:
+        print("the two condition are false")
+else:
+    print("false")'''
+
+'''a=int(input('data'))
+b=int(input('data'))
+if a==b:
+    print("true")
+    if a<b:
+        print("false")
+    else:
+        print("the two condition are false")
+else:
+    print("false")'''
+#logical
+'''a=6
+b=8
+if a==b and a>b:
+    print("true")
+    if a<b or b>a:
+        print("false")
+    else:
+        print("the two condition are false")
+else:
+    print("false")'''
+
+
+
+
+'''a=int(input("values:"))
+b=int(input("value:"))
+if a!=b and a<b:
+    print("true")
+    if a>b or b==a:
+        print("false")
+    else:
+        print("the two condition are false")
+else:
+    print("false")'''
+
+#identify
+
+'''a=6
+b=8
+if type(a) is str:
+    print("true")
+    if type(b) is not int:
+        print("false")
+    else:
+        print("the two condition are false")
+else:
+    print("false")'''
+
+'''a=int(input("data:"))
+b=int(input("data:"))
+if type(a) is int:
+    print("true")
+    if type(b) is not int:
+        print("false")
+    else:
+        print("the two condition are false")
+else:
+    print("false")'''
+#membership
+
+'''a=[10,20,30,40]
+b=[10]
+if b in a :
+    print("yes")
+    if b not in a:
+        print("may be")
+    else:
+            print("this is wrong")
+else:
+    print("false")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
