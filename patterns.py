84100143a53a270909083a91407065dc2712ca0a
divviprameela-ui <divviprameela@gmail.com>
2026-06-12 19:35:04 +0530

    Add files via upload

---
+++ patterns.py
@@ -0,0 +1,16 @@
+#patterns
+#right angle triangle
+
+n = int(input("Enter number of rows: "))
+for i in range(1, n + 1):
+    print("*" * i)
+#reverse tringle
+    
+n = int(input("Enter number of rows: "))
+for i in range(n, 0, -1):
+    print("*" * i)
+
+#square
+n = int(input("Enter number of rows: "))
+for i in range(n):
+    print("*" * n)
---
+++ task 7.py
@@ -0,0 +1,36 @@
+#task
+'''def cal():
+    a=int(input("a value:"))
+    b=int(input("b value:"))
+    option=int(input("1.add 2.sub 3.mutli:"))
+    if option==1:
+        print("add:",a+b)
+    elif option==2:
+        print("sub:",a-b)
+    elif option==3:
+        print("multi:",a*b)
+cal()'''
+
+#split bill
+'''def cal():
+    person=int(input("How Many Persons:"))
+    amount=10000
+    bill_per_person=amount/person
+    print("the amount per person:",bill_per_person)
+cal()'''
+#format()
+def cal():
+    person=int(input("How Many Persons:"))
+    amount=10000
+    bill_per_person=amount/person
+    print("the amount per person{}".format(bill_per_person))
+cal()
+
+#f format
+def cal():
+    person=int(input("How Many Persons:"))
+    amount=10000
+    bill_per_person=amount/person
+    print(f"the amount per person{bill_per_person}")
+cal()'''
+
