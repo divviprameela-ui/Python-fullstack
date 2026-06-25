3fc842a9d195cfcb95904d2af0eb0cc2583c95b5
divviprameela-ui <divviprameela@gmail.com>
2026-06-25 15:55:53 +0530

    Add files via upload

---
+++ task9.py
@@ -0,0 +1,14 @@
+#BMI
+'''while True:
+    weight=eval(input("enter the weight:"))
+    height=eval(input("enter the height:"))
+    bmi=weight/(height**2)
+    print("BMI:",bmi)
+    if bmi<18.5:
+        print("your under weight")
+    elif bmi>18.5 and bmi<24.5:
+        print("your normal weight")
+    elif bmi>=24.5 and bmi<29.5:
+        print("your over weight")
+    else:
+        print("obesity")'''
